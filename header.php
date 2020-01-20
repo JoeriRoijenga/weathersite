75fa9a4ecb4a6a30bde16fcb785091aac3d324a5
Niels de Vries <nielsdevries@riseup.net>
2020-01-20 13:29:08 +0100

    fixed layout and markup

---
+++ header.php
@@ -0,0 +1,39 @@
+<style>
+@font-face {
+	font-family: ABCSans-Black;
+	src:	url('res/ABCSans-Black.woff2') format('woff2'),
+			url('res/ABCSans-Black.ttf') format('ttf');
+	font-weight: bold;
+}
+@font-face {
+	font-family: ABCSans;
+	src:	url(res/ABCSans.woff2) format('woff2'),
+			url(res/ABCSans.ttf) format('ttf');
+	font-weight: normal;
+}
+@font-face {
+	font-family: ABCSans;
+	src:	url(res/ABCSans-Bold.woff2) format('woff2'),
+			url(res/ABCSans-Bold.ttf) format('ttf');
+	font-weight: bold;
+}
+
+.header {
+    padding-left: 10px;
+    text-align: left;
+    background: white;
+    color: black;
+}
+
+.logotext {
+	font-family: ABCSans-Black;
+	font-size: 80px;
+	display: inline-block;
+	padding-left: 7px;
+}
+.logoimg {
+	display: inline-block;
+}
+</style>
+
+<div class="logoimg"><img src="res/abclogo.svg" alt="logo" height="60"></div><div class="logotext">ABC</div>
