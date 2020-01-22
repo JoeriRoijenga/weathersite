8eb35dd3e1efd7083718da2ac27494dda09246db
Niels de Vries <nielsdevries@riseup.net>
2020-01-22 14:57:55 +0100

    fix: styled login window

--- login.php
+++ login.php
@@ -2,38 +2,73 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>ABCWeather: Login</title>
+	<title>ABC: Weatherportal-Login</title>
+	<link rel="stylesheet" type="text/css" href="font.css">
+	<style>
+		* {
+			font-family: ABCSans;
+		}
+		body, html {
+  			height: 100%;
+  			display: grid;
+		}
+		#login-box {
+			margin: auto;
+		}
+		#form-box {
+			padding: 20px;
+  			
+  			background-color: #d9d9d9;
+  			border-radius: 10px;
+   		}
+
+   		#logo-text {
+			font-family: ABCSans-Black;
+			font-size: 80px;
+			display: inline-block;
+			padding-left: 7px;
+		}
+		#logo-img {
+			display: inline-block;
+		}
+
+		h1 {
+			font-family: ABCSans-Bold;
+		}
+	</style>
 </head>
 <body>
-	<?php
-	include 'database.php';
-	if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
-  		$input_username = $_POST['username'];
-  		$input_password = $_POST['password'];
+<?php
+include 'database.php';
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+	// todo: input sanitizen!
+	$input_username = $_POST['username'];
+	$input_password = $_POST['password'];
 
-		$sql = "SELECT user_id, username, password, priv_level FROM users WHERE username = '$input_username'";
-  		$result = mysqli_query($db, $sql);
-  		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-  	
-  		if (password_verify($input_password, $row["password"])) {
-    		
-    		session_start();
-    		$_SESSION['username'] = $row['username'];
-    		$_SESSION['priv_level'] = $row['priv_level'];
-    		header("Location: ../index.php");
-    	} else {
-    		echo 'Invalid password.';
-    	}
-		
-	};
-	?>
-	<h2>Login:</h2>
+	$sql = "SELECT user_id, username, password, priv_level FROM users WHERE username = '$input_username'";
+	$data = mysqli_fetch_array(mysqli_query($db, $sql),MYSQLI_ASSOC);
+	if (password_verify($input_password, $data["password"])) {
+   		session_start();
+   		$_SESSION['username'] = $data['username'];
+   		$_SESSION['priv_level'] = $data['priv_level'];
+   		header("Location: ../index.php");
+   	} else {
+   		echo 'Invalid password.';
+   	}		
+};
+?>
+<div id="login-box">
+<div id="logo-img"><img src="res/abclogo.svg" alt="logo" height="60"></div><div id="logo-text">ABC</div>
+<div id="form-box">
+	<h1>Login</h1>
 	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <label for="username">Username:</label>
-    <input type="text" name="username" placeholder="Enter username">
-    <label for="password">Password:</label>
-    <input type="password" name="password" placeholder="Enter password">
-    <button type="submit">Log In</button>
+		<label>Username:</label><br>
+		<input type="text" name="username" placeholder="Enter username"><br>
+		<label>Password:</label><br>
+		<input type="password" name="password" placeholder="Enter password"><br><br>
+		<button type="submit">Log In</button>
 	</form>
+</div>
+</div>
 </body>
 </html>
