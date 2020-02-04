16cfd49596daa5916d0a5b94ed38cf9c09eed6f4
Niels de Vries <nielsdevries@riseup.net>
2020-02-04 14:41:42 +0100

    add top10-

--- Controllers/PageController.php
+++ Controllers/PageController.php
@@ -16,7 +16,21 @@ class PageController extends Controller
 
     public function precipitation()
     {
-        $this->html("precipitation");
+        $this->html("precipitation", 
+            ["topTen" => $this->createArray()]);
+    }
+
+    private function createArray() {
+        $ch = curl_init();
+        $url = $_SERVER['HTTP_HOST'] . "/api/v1/weather/latest";
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($ch); 
+        curl_close($ch);
+
+        $object = json_decode($data);
+
+        return $object->items;
     }
 
     public function map()
