4d20e90837b56050b8d638b4577e7c773dcd8b38
joeriroijenga <roijenga.joeri@gmail.com>
2020-02-04 11:48:09 +0100

    Style fixes met overige files

--- views/admin/home.php
+++ views/admin/home.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html>
-<head>
- 	<meta charset="utf-8">
-	<title>ABCWeather: Admin</title>
-</head>
-<body>
 <a href='/admin/user/add'>Add user</a><br>
 <a href='/admin/users'>List users</a>
-</body>
-</html>
-
--- views/precipitation.php
+++ views/precipitation.php
@@ -1,50 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>ABC: Weatherportal</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-* {
-    box-sizing: border-box;
-}
-
-body {
-    font-family: Arial, Helvetica, sans-serif;
-    margin: 0;
-}
-
-/* Column container */
-.row {  
-    display: flex;
-    flex-wrap: wrap;
-}
-
-/* Create two unequal columns that sits next to each other */
-/* Sidebar/left column */
-.side {
-    flex: 20%;
-    background-color: #f1f1f1;
-    padding: 20px;
-}
-
-/* Main column */
-.main {   
-    flex: 70%;
-    background-color: white;
-    padding: 20px;
-}
-
-/* Responsive layout - when the screen is less than 700px wide, make the two columns stack on top of each other instead of next to each other */
-@media screen and (max-width: 700px) {
-    .row {   
-        flex-direction: column;
-    }
-}
-</style>
-</head>
-<body>
-
 <div class="row">
   <div class="side">
       <h2>Sidebar</h2>
@@ -56,5 +9,3 @@ body {
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in porta est. In ac rutrum mauris, vel tincidunt orci. Sed gravida erat pharetra nulla posuere suscipit. Phasellus non egestas justo, vitae ultricies purus. Quisque luctus viverra augue quis placerat. In posuere quis risus quis ultrices. Quisque nec enim sollicitudin, molestie arcu id, bibendum lacus. Maecenas varius diam at eros posuere, eget semper mi commodo. Nulla semper libero in faucibus convallis. Curabitur hendrerit magna sapien, nec tempor turpis aliquet eget. Aenean ac malesuada odio, eu dapibus libero. Nulla porta purus et elit scelerisque, sit amet sodales eros tincidunt. </p>
   </div>
 </div>
-</body>
-</html>
