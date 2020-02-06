4eee4ed6fd3937ff2927a92f2e7143ee90424cc8
MartijnVisser98 <m.visser.2@hotmail.com>
2020-02-06 15:06:03 +0100

    Changed session username and prive level location

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -69,6 +69,12 @@ abstract class Controller
         }
 
         $hasHistorical = file_exists(__DIR__ . '/../assets/historical/' . date('Y-m-d') . '.xml');
+
+        if (!empty($_SESSION)){
+            $username = $_SESSION['username'];
+            $priv_level = $_SESSION['priv_level'];
+        }
+
         include __DIR__ . '/../views/sections/header.html';
         if ($file != "login") {
             include __DIR__ . '/../views/sections/navbar.php';
